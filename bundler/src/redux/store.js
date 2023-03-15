05c55819e0d42720cb7d1fe878fb440dd8ba8833
LAPTOP-GD7ICC6K <sbsangu1412gmail.com>
2023-03-15 11:19:34 +0530

    mongo bug fix

--- bundler/src/redux/store.js
+++ bundler/src/redux/store.js
@@ -1,15 +1,12 @@
-import {configureStore} from "@reduxjs/toolkit"
+import { configureStore } from '@reduxjs/toolkit';
 
-import {userReducer} from "./reducers/userReducer"
-const store=configureStore({
-reducer:{
-    user:userReducer,
-
-
-}
-
-})
+import { userReducer } from './reducers/userReducer';
+const store = configureStore({
+  reducer: {
+    user: userReducer,
+  },
+});
 
 export default store;
 
-export const server="https://course-liart.vercel.app";
+export const server = 'https://courze-api.vercel.app';
--- server/config/database.js
+++ server/config/database.js
@@ -2,7 +2,9 @@ import mongoose from "mongoose"
 export const connectDB=async()=>{
 
     try {
+        mongoose.set("strictQuery", false);
         const { connection } = await mongoose.connect(process.env.MONGO_URI)
+        console.log("perfectly running")
       } catch (error) {
         console.log(error);
       }
