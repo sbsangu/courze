52ead772c3b3fd4fe9e2de6337c7f72e1da05170
LAPTOP-GD7ICC6K <sbsangu1412gmail.com>
2023-03-15 11:04:09 +0530

    some random stuffs

--- server/config/database.js
+++ server/config/database.js
@@ -1,10 +1,8 @@
 import mongoose from "mongoose"
 export const connectDB=async()=>{
-   
 
     try {
-        const { connection } = await mongoose.connect(process.env.MONGO_URI);
-        console.log(`MongoDB connected with ${connection.host}`);
+        const { connection } = await mongoose.connect(process.env.MONGO_URI)
       } catch (error) {
         console.log(error);
       }
