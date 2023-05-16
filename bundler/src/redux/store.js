13b90b53a67931d6b9eb31438d20ab4369287478
LAPTOP-GD7ICC6K <sbsangu1412gmail.com>
2023-05-16 20:28:02 +0530

    changes

--- bundler/src/redux/store.js
+++ bundler/src/redux/store.js
@@ -2,7 +2,7 @@ import { configureStore } from '@reduxjs/toolkit';
 
 import { userReducer } from './reducers/userReducer';
 
-export const server = 'http://localhost:4000/api/v1/';
+export const server = 'https://courze-api.vercel.app/api/v1/';
 const store = configureStore({
   reducer: {
     user: userReducer,
